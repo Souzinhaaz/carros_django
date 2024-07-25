b11e6281c6404f438cdcaa7edbe8b76d26849652
Souza <114781129+Souzinhaaz@users.noreply.github.com>
2024-07-24 22:25:41 -0300

    aula 77

--- cars/apps.py
+++ cars/apps.py
@@ -1,6 +1,9 @@
-from django.apps import AppConfig
+from django.apps import AppConfig # type: ignore
 
 
 class CarsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cars'
+
+    def ready(self):
+        import cars.signals
